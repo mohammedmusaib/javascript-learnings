7131f2bc81277c01b34efa32fc24433a627f8c0e
mohammedmusaib <mymusaib50@gmail.com>
2021-07-25 22:21:29 +0530

    learning Javascript

---
+++ index.js
@@ -0,0 +1,43 @@
+// var a=10;
+// var b=20;
+// console.log(a+b);       
+// console .log(typeof a)    
+// console .log(typeof b)
+
+// var c="Mohammed";
+// console.log(c);
+// console .log(typeof c )
+
+// var d=true;   
+// console.log(d)
+
+
+// function print(){
+//     console.log("All ways be Happy");
+// }
+// print()
+
+
+// function add(c,d){
+//     var s=c+d;
+//     return s;
+// }
+// var res = add(6,8);
+// console.log(res);
+
+// 
+
+ function md(){
+ console.log("Ahmed");
+}
+ md()
+function mohammed(a,b,c){
+    var R=(a-b-c);
+    return R;
+}
+var res=mohammed(10,10,20);
+console.log(res);
+
+
+
+
